f327eb2ee19bc8f6155771d34e7661de2459d6b9
sudikrt <sudarshankaranth18@gmail.com>
2015-12-17 09:25:35 +0530

    /*Client_server demo*/
    Signed-off-by: sudikrt <sudarshankaranth18@gmail.com>

--- Client_server_demo/client_send_request.c
+++ Client_server_demo/client_send_request.c
@@ -14,45 +14,47 @@ int request (int client_fd, char * file_name, char * dest)
         if (ret != 0)
         {
             do{
-                ret = read (client_fd, &success, sizeof(success));
-                if(ret != 0)
-                {
-                    switch(success)
+                    ret = read (client_fd, &success, sizeof(success));
+                    if(ret != 0)
                     {
-                        /*If file not exist in server*/
-                        case ENOENT:
-                                    fprintf (stderr, "\tERROR: %s\n", strerror(ENOENT));
-                                    ret = -1;
-                                    exit_var = 0;
-                                    goto out;
-                                    break;
-                        /*If File read fail */
-                        case read_fail:
-                                    printf ("\tERROR: Read Fail\n");
-                                    exit_var = 0;
-                                    ret = -1;
-                                    goto out;
-                                    break;
-                        /*Send the acknowledgment to server that it is ready to 
-                         * recieve the file*/
-                        case s_accept:
-                                    ret = write (client_fd, file_name, len);
-                                    break;
-                        /*If file exists it will recieve the file from the 
-                         * server*/ 
-                        case file_exist:
-                                    ret = get_doc_from_server(client_fd,
-                                                            file_name, dest);
-                                    if(ret < 0)
-                                    {
-                                        printf ("\tError in saving the file\n");
+                        switch(success)
+                        {
+                            /*If file not exist in server*/
+                            case ENOENT:
+                                        fprintf (stderr, "\tERROR: %s\n", 
+                                                            strerror(ENOENT));
+                                        ret = -1;
+                                        exit_var = 0;
                                         goto out;
-                                    }
-                                    printf ("\tFile Successfully saved\n");
-                                    exit_var = 0;
-                                    break;
+                                        break;
+                            /*If File read fail */
+                            case read_fail:
+                                        printf ("\tERROR: Read Fail\n");
+                                        exit_var = 0;
+                                        ret = -1;
+                                        goto out;
+                                        break;
+                            /*Send the acknowledgment to server that it is ready to 
+                             * recieve the file*/
+                            case s_accept:
+                                        ret = write (client_fd, file_name, len);
+                                        break;
+                            /*If file exists it will recieve the file from the 
+                             * server*/ 
+                            case file_exist:
+                                        ret = get_doc_from_server(client_fd,
+                                                                file_name, dest);
+                                        if(ret < 0)
+                                        {
+                                                printf ("\tError in saving 
+                                                                the file\n");
+                                                goto out;
+                                        }
+                                        printf ("\tFile Successfully saved\n");
+                                        exit_var = 0;
+                                        break;
+                        }
                     }
-                }
             }while (exit_var == 1);
             
         }
@@ -61,5 +63,4 @@ int request (int client_fd, char * file_name, char * dest)
         
         out:
             return ret;
-        
 }
